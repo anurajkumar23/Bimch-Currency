8b2f7edf0840d172c0a2d17bedf839289981f4d5
Puskar-Roy <puskarroy600@gmail.com>
2023-08-25 16:17:31 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,13 @@
-// const express = require('express');
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const cokkie = require("cookie-parser");
-// const route = require('./routes/routes');
-
-// const app = express();
-
-
-// const allowedOrigins = [
-//   "https://bimch.vercel.app/",
-// ];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-
-// app.use(cokkie());
-// app.use(bodyParser.json({ limit: "1024mb" }));
-// app.use("/api",route);
-// app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
-// dotenv.config();
-// require('./database/connectDatabase');
-
-
-// app.listen(process.env.PORT,()=>{
-//     console.log("⚡[Server] Server Start At Port " + process.env.PORT);
-// })
-
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const cors = require("cors"); // Import the cors package
 const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser"); // Corrected the spelling here
+const cookieParser = require("cookie-parser");
 const route = require("./routes/routes");
 
 const app = express();
 
-const allowedOrigins = ["https://bimch.vercel.app"];
+const allowedOrigins = ["https://bimch.vercel.app"]; // Adjust the allowed origins
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -61,12 +20,15 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "1024mb" }));
+app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
+
+// Use the cors middleware with your configured options
 app.use(cors(corsOptions));
 
-app.use(cookieParser()); // Corrected the spelling here
-app.use(bodyParser.json({ limit: "1024mb" }));
 app.use("/api", route);
-app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
+
 dotenv.config();
 require("./database/connectDatabase");
 
