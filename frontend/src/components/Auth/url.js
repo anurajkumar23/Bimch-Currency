d7d564ef2c3394f99ab61f4dd427caba7d866054
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-09-03 17:43:42 +0530

    changes

--- frontend/src/components/Auth/url.js
+++ frontend/src/components/Auth/url.js
@@ -1,2 +1,2 @@
-export const url = "https://server-huil.onrender.com"; // for deploy
+export const url = "https://server-huil.onrender.com";
 // export const url = "http://localhost:8080"; //for local host
